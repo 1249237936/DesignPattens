b54bab868609b9a4a2ea8f31306a97bca20832fe
Jason <15521215849@163.com>
2021-06-07 10:49:08 +0800

    单利模式

---
+++ src/com/json/designpattens/Mgr06.java
@@ -0,0 +1,34 @@
+package com.json.designpattens;
+
+public class Mgr06 {
+    private Mgr06() {
+
+    }
+    private static Mgr06 INSTANCE = null;
+
+    public static Mgr06 getInstance() {
+        if (INSTANCE == null) {
+            synchronized (Mgr06.class) {
+                if (INSTANCE == null) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    INSTANCE = new Mgr06();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+
+            new Thread(() -> {
+                System.out.println(Mgr06.getInstance().hashCode());
+            }).start();
+
+        }
+    }
+}
