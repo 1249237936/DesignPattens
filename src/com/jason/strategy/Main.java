0d436c9b79031c3092fae27a525bdfc488182ccd
Jason <15521215849@163.com>
2021-06-07 18:16:07 +0800

    Strategy

--- src/com/jason/strategy/Main.java
+++ src/com/jason/strategy/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Cat[] a = {new Cat(3, 3), new Cat(5, 5), new Cat(1, 1)};
         //Dog[] a = {new Dog(3), new Dog(5), new Dog(1)};
         Sorter<Cat> sorter = new Sorter<>();
-        sorter.sort(a,new CatHeightComparator());
+        sorter.sort(a,(o1, o2) -> Integer.compare(o1.weight, o2.weight));
         System.out.println(Arrays.toString(a));
     }
 }
