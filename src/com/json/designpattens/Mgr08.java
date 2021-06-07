6123744469cb5b4d4dd0ec83c3ec4651374ee2ae
Jason <15521215849@163.com>
2021-06-07 11:36:29 +0800

    单例模式——枚举

--- src/com/json/designpattens/Mgr08.java
+++ src/com/json/designpattens/Mgr08.java
@@ -1,2 +1,13 @@
-package com.json.designpattens;public class Mgr08 {
+package com.json.designpattens;
+
+public enum Mgr08 {
+    INSTANCE;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                System.out.println(Mgr08.INSTANCE.hashCode());
+            }).start();
+        }
+    }
 }
