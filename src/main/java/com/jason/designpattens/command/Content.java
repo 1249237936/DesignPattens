949de0c461072283cdbb3301e81125b28e35253e
Jason <15521215849@163.com>
2021-07-08 23:25:01 +0800

    command

---
+++ src/main/java/com/jason/designpattens/command/Content.java
@@ -0,0 +1,5 @@
+package com.jason.designpattens.command;
+
+public class Content {
+    String msg = "hello everybody";
+}
---
+++ src/main/java/com/jason/designpattens/command/InsertCommand.java
@@ -0,0 +1,17 @@
+package com.jason.designpattens.command;
+
+public class InsertCommand extends Command{
+    Content c;
+    String strToInsert = "http://www.mashibing.com";
+    public InsertCommand(Content c) {this.c = c;}
+
+    @Override
+    public void doit() {
+        c.msg = c.msg + strToInsert;
+    }
+
+    @Override
+    public void undo() {
+        c.msg = c.msg.substring(0, c.msg.length() - strToInsert.length());
+    }
+}
