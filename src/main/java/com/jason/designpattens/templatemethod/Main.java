fd4d3aa061ae2849db77719e4cc0bfeae0a7a653
Jason <15521215849@163.com>
2021-07-11 13:58:44 +0800

    模板方法

--- src/main/java/com/jason/designpattens/templatemethod/Main.java
+++ src/main/java/com/jason/designpattens/templatemethod/Main.java
@@ -1,2 +1,31 @@
-package com.jason.designpattens.templatemethod;public class Main {
+package com.jason.designpattens.templatemethod;
+
+public class Main {
+    public static void main(String[] args) {
+        F f = new C1();
+        f.m();
+    }
+}
+
+abstract class F {
+    public void m() {
+        op1();
+        op2();
+    }
+
+    protected abstract void op1();
+    protected abstract void op2();
+
+}
+
+class C1 extends F {
+    @Override
+    protected void op1() {
+        System.out.println("op1");
+    }
+
+    @Override
+    protected void op2() {
+        System.out.println("op2");
+    }
 }
